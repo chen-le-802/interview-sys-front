32c1b69a02a97e93b1544f607e80751847496c8e
ChenJ <3233715564@qq.com>
2025-04-21 21:10:34 +0800

    init Project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <HomeView />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HomeView from '@/views/HomeView.vue'
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        HomeView
+    }
+})
+</script>
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'HomeView'
+})
+</script>
+
+<template>
+    <div class="box">测试</div>
+</template>
+
+<style scoped>
+.box {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(180, 163, 163);
+}
+</style>
